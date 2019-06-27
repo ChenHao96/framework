4b71c52e18cf2c7165455e00b583b25b0600792e
StevenChen <949727157@qq.com>
2019-06-27 16:26:55 +0800

    update

--- framework-web/src/main/java/org/steven/chen/web/AbstractController.java
+++ framework-web/src/main/java/org/steven/chen/web/AbstractController.java
@@ -1,9 +1,11 @@
 package org.steven.chen.web;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.steven.chen.utils.CommonsUtil;
-import org.steven.chen.utils.IOUtils;
-import org.steven.chen.utils.RequestParamUtil;
+import org.steven.chen.utils.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,8 +14,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 public abstract class AbstractController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractController.class);
 
     public static final String CLIENT_IP_KEY = "AbstractController_CLIENT_IP";
     public static final String CLIENT_PORT_KEY = "AbstractController_CLIENT_PORT";
@@ -70,5 +75,45 @@ public abstract class AbstractController {
         } else {
             response.setStatus(404);
         }
+    }
+
+    protected Map<String, String> param2MapStrParamJson() {
+        try {
+            String jsonParam = RequestParamUtil.readRequestContent(getRequest());
+            LOGGER.info("param2MapStrParamJson :{}", jsonParam);
+            if (StringUtil.isEmpty(jsonParam)) return null;
+            JsonNode jsonNode = JsonUtils.jsonStr2JsonNode(jsonParam);
+            return BeanMapConvertUtil.bean2StringMap(jsonNode);
+        } catch (IOException e) {
+            LOGGER.warn("param2MapStrParamJson", e);
+        }
+        return null;
+    }
+
+    protected Map<String, String> param2MapStrParam() {
+        Map<String, String> params = RequestParamUtil.getRequestParam(getRequest());
+        if (CollectionUtils.isEmpty(params)) {
+            String contentType = getRequest().getContentType();
+            if (StringUtil.isNotEmpty(contentType)) {
+                if (contentType.toLowerCase().contains("json")) {
+                    params = param2MapStrParamJson();
+                } else if (contentType.toLowerCase().contains("xml")) {
+                    params = param2MapStrParamXml();
+                }
+            }
+        }
+        return params;
+    }
+
+    protected Map<String, String> param2MapStrParamXml() {
+        try {
+            String xmlParam = RequestParamUtil.readRequestContent(getRequest());
+            LOGGER.info("param2MapStrParamXml :{}", xmlParam);
+            if (StringUtil.isEmpty(xmlParam)) return null;
+            return XmlParserUtils.xmlToMap(xmlParam);
+        } catch (Exception e) {
+            LOGGER.warn("param2MapStrParamXml", e);
+        }
+        return null;
     }
 }
