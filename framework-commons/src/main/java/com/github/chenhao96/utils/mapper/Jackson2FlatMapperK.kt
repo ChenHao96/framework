2e04df166a7927d23a02369d477e4cc027298ee1
StevenChen <949727157@qq.com>
2019-07-09 17:56:16 +0800

    update

--- framework-commons/src/main/java/com/github/chenhao96/utils/mapper/Jackson2FlatMapperK.kt
+++ framework-commons/src/main/java/com/github/chenhao96/utils/mapper/Jackson2FlatMapperK.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.steven.chen.utils.mapper
+package com.github.chenhao96.utils.mapper
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.core.JsonParser
